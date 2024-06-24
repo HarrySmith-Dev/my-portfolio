0750a69d493744ec3a73cbba8c55aca636b89437
HarrySmith-Dev <harry.smith0206@gmail.com>
2024-06-24 20:28:56 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { colors } from "./shared";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ color: `${colors.red}` }}>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
