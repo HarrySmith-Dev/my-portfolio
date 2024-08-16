d6d381517e76c802b7894689e9679f46a35ffd35
HarrySmith-Dev <harry.smith0206@gmail.com>
2024-08-16 22:06:50 +0100

    fixed app

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import TopNavigation from "./components/navigation/TopNavigation";
 
 const App = () => (
-  <div>
+  <>
     <TopNavigation />
-  </div>
+  </>
 );
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import './index.css'
+import "./styles.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App.tsx'
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+ReactDOM.createRoot(document.getElementById("main")!).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
